5826a88db7c4037ecb26c9b278f4979a5e26922a
Alanna <phan.alanna@gmail.com>
2023-11-25 15:22:57 -0500

    create backend rails folder

---
+++ frontend/src/hooks/reducer.js
@@ -0,0 +1,39 @@
+import { pets } from "../mocks/pets";
+
+export const ACTIONS = {
+  SET_EVENT_DATA: 'SET_EVENT_DATA',
+  SET_USER_DATA: 'SET_USER_DATA',
+  SET_DAY_DATA: 'SET_DAY_DATA',
+  SET_ENERGY_DATA: 'SET_ENERGY_DATA',
+  SET_PETS_DATA: 'SET_PETS_DATA',
+  PERFORM_ACTION: 'PERFORM_ACTION',
+  SLEEP: 'SLEEP'
+}
+
+export function reducer(state, action) {
+  switch (action.type) {
+
+    case 'SET_EVENT_DATA':
+      return { ...state, event: action.value };
+
+    case 'SET_USER_DATA':
+      return { ...state, user: action.value };
+
+    case 'SET_DAY_DATA':
+        return { ...state, day: action.value };
+
+    case 'SET_ENERGY_DATA':
+      return { ...state, energy: action.value };
+
+    case 'PERFORM_ACTION':
+      return { ...state, pets:[...pets, pets[0].mood = action.value], energy: state.energy - 1 };
+
+    case 'SLEEP':
+      return { ...state, energy: 5, day: state.day + 1 };
+
+    default:
+    throw new Error(
+      `Tried to reduce with unsupported action type: ${action.type}`
+    )
+  }
+}
---
+++ frontend/src/mocks/pets.js
@@ -0,0 +1,24 @@
+export const pets = [
+  {
+  id: 1,
+  species: "dog",
+  name: "Fido",
+  mood: 3,
+  user: 1,
+  },
+  {
+  id: 2,
+  species: "cat",
+  name: "Garfield",
+  mood: 3,
+  user: 1,
+  },
+  {
+    id: 3,
+    species: "bearded lizard",
+    name: "Spike",
+    mood: 3,
+    user: 1,
+  },
+
+];
