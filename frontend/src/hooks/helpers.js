577a69890412f22736df0e2479efb7d24e7178ee
amandadr <amandadroy@gmail.com>
2023-12-01 16:51:16 -0400

    move helper data and remove dev mocks

--- frontend/src/hooks/helpers.js
+++ frontend/src/hooks/helpers.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { ACTIONS } from "./reducer";
+import { petsData } from "../data/pets";
 
 export function getBySpecies(species, array) {
   return array.find((pet) => pet.species === species);
@@ -55,45 +56,6 @@ export const createGame = (input, dispatch) => {
       });
       return data.id;
     });
-
-  const petsData = [
-    {
-      species: "Wolf",
-      name: "Fang",
-      mood: 5,
-      treat: 1,
-      play: 2,
-      talk: 0,
-      to_pet: -1,
-      pet_happy: "http://localhost:3001/images/sprites/wolf_happy.png",
-      pet_sad: "http://localhost:3001/images/sprites/wolf_sad.png",
-      pet_neutral: "http://localhost:3001/images/sprites/wolf_neutral.png",
-    },
-    {
-      species: "Cat",
-      name: "Noctis",
-      mood: 5,
-      treat: 2,
-      play: -1,
-      talk: 1,
-      to_pet: 0,
-      pet_happy: "http://localhost:3001/images/sprites/cat_happy.png",
-      pet_sad: "http://localhost:3001/images/sprites/cat_sad.png",
-      pet_neutral: "http://localhost:3001/images/sprites/cat_neutral.png",
-    },
-    {
-      species: "Slime",
-      name: "Wiggy",
-      mood: 5,
-      treat: 1,
-      play: 2,
-      talk: 1,
-      to_pet: 1,
-      pet_happy: "http://localhost:3001/images/sprites/slime_happy.png",
-      pet_sad: "http://localhost:3001/images/sprites/slime_neutral.png",
-      pet_neutral: "http://localhost:3001/images/sprites/slime_neutral.png",
-    },
-  ];
 
   const petRequests = petsData.map((pet) => {
     return axios.post("/pets.json", { pet });
