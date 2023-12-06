619692ad4acd69787bd9ca72076daecb78f4dfdd
amandadr <amandadroy@gmail.com>
2023-12-05 20:07:35 -0400

    again

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -28,6 +28,7 @@ function App() {
   
     //    return
     //  }
+    const userAgent = navigator.userAgent.toLowerCase();
     let touchEvent = (userAgent.indexOf('iphone') != -1) ? "tap" : "click";
 
     const onClick = (e) => {
