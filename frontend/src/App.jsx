0fab2aea7f15a74196ccbf163d7e2ec75719a5a9
amandadr <amandadroy@gmail.com>
2023-12-05 20:19:46 -0400

    try again

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -28,7 +28,7 @@ function App() {
   
     //    return
     //  }
-    let touchEvent = "ontouchstart" in window ? "onmousedown" : "click";
+    let touchEvent = "onpointerdown" in window ? "onmousedown" : "click";
 
     const onClick = (e) => {
       if((e.pointerType === "mouse" || e.pointerType === "touch") && screen < 3){
